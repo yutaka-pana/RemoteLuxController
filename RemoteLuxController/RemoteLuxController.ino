9549aed0dc5dc07ed008fbede52e78b63153b3cb
Yutaka@Panasonic <yamamoto.yutaka@jp.panasonic.com>
2022-09-02 17:02:15 +0900

    Repository Preparation

--- RemoteLuxController/RemoteLuxController.ino
+++ RemoteLuxController/RemoteLuxController.ino
@@ -1,3 +1,9 @@
+// Author: Yutaka Yamamoto
+// Github Repo: https://github.com/yutaka-pana/RemoteLuxController.git
+// Description: IP controlled automatic lux controller.
+//              Get environment light strength from Lux sensor and 
+//              controll external lights to meet ordered Lux parameter.
+
 void setup() {
   // put your setup code here, to run once:
 
